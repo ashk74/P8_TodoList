54059104f46a0cebb588f9b9f965c8152012377a
Jonathan Secher <secher.jonathan@gmail.com>
2022-05-04 23:02:01 +0200

    First commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    private EntityManagerInterface $em;
+    private UserRepository $userRepository;
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->em = $em;
+        $this->userRepository = $userRepository;
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    #[Route('/users', name: 'user_list')]
+    public function listAction()
+    {
+        return $this->render('user/list.html.twig', ['users' => $this->userRepository->findAll()]);
+    }
+
+    #[Route('/users/create', name: 'user_create')]
+    public function createAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->userPasswordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($password);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/users/{id}/edit', name: 'user_edit')]
+    public function editAction(User $user, Request $request)
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->userPasswordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($password);
+
+            $this->em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
