d84d922051a875b4c0eb5e751e657e1d878afdab
Jonathan Secher <secher.jonathan@gmail.com>
2022-05-30 23:57:56 +0200

    Move coverage into public folder + Add UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $user = new User();
+        $user->setEmail('johndoe@email.com')
+            ->setUsername('John.Doe')
+            ->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail($faker->freeEmail())
+                ->setUsername($faker->userName())
+                ->setPassword($this->passwordHasher->hashPassword($user, '123'));
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
